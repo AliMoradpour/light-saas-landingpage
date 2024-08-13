eafab80f6f77f512264e5a7e092530d51eeabba5
Ali <86645460+AliMoradpour@users.noreply.github.com>
2024-08-13 23:06:22 +0330

    initial project

---
+++ src/sections/Header.tsx
@@ -0,0 +1,10 @@
+import ArrowRight from "@/assets/arrow-right.svg"
+
+export const Header = () => {
+  return (
+    <div className="flex justify-center items-center py-3 bg-black text-white text-sm">
+      <p>Get Started for free</p>
+      <ArrowRight classNmae="h-4 w-4 inlineflex" />
+    </div>
+  );
+};
