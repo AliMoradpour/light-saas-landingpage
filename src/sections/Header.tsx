e2ef463f3bd6f4b54d0c278eb29b5bd7f4796d3c
Ali <86645460+AliMoradpour@users.noreply.github.com>
2024-10-05 22:16:23 +0330

    hero section completed

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -6,11 +6,9 @@ import Image from "next/image";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0">
+    <header className="sticky top-0 backdrop-blur-sm z-20">
       <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
-        <p className="text-white/60 hidden md:block">
-          Streamline your workflow and boost your productivity
-        </p>
+        <p className="text-white/60 hidden md:block">Streamline your workflow and boost your productivity</p>
         <div className="inline-flex gap-1 items-center">
           <p>Get Started for free</p>
           <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
