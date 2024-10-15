7714bf0ed1f32a31fd9dd79f5811318274e3d92c
Ali <86645460+AliMoradpour@users.noreply.github.com>
2024-10-15 20:25:16 +0330

    product showcase section animated

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,20 +1,30 @@
-import Image from "next/image";
+"use client";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 import productImage from "@/assets/product-image.png";
 import pyramidImage from "@/assets/pyramid.png";
 import tubeImage from "@/assets/tube.png";
+import { useRef } from "react";
+import Image from "next/image";
 
 export const ProductShowcase = () => {
+  const sectionRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
   return (
-    <section className="bg-gradient-to-b from-[#fff] to-[#D2DCFF] py-24 overflow-x-clip">
+    <section ref={sectionRef} className="bg-gradient-to-b from-[#fff] to-[#D2DCFF] py-24 overflow-x-clip">
       <div className="container">
         <div className="section-heading">
           <div className="flex justify-center">
             <div className="tag">Boost your productivity</div>
           </div>
-          <h2 className="section-title mt-5">
-            A more effective way to track progress
-          </h2>
+          <h2 className="section-title mt-5">A more effective way to track progress</h2>
           <p className="section-description mt-5">
             Effortlessly turn your ideas into a fully functional, responsive, no-code SaaS website in just minutes with
             the set of free components for Framer.
@@ -22,14 +32,26 @@ export const ProductShowcase = () => {
         </div>
         <div className="relative">
           <Image src={productImage} alt="Product image" className="mt-10" />
-          <Image
-            src={pyramidImage}
+          <motion.img
+            src={pyramidImage.src}
             alt="pyramid image"
-            className="hidden md:block absolute -right-36 -top-32"
             height={262}
             width={262}
+            className="hidden md:block absolute -right-36 -top-32"
+            style={{
+              translateY,
+            }}
           />
-          <Image src={tubeImage} alt="tube image" className="hidden md:block absolute bottom-24 -left-36" height={248} width={248} />
+          <motion.img
+            src={tubeImage.src}
+            alt="tube image"
+            height={248}
+            width={248}
+            className="hidden md:block absolute bottom-24 -left-36"
+            style={{
+              translateY,
+            }}
+          />
         </div>
       </div>
     </section>
